670bb6244e58f7b600cb633d51e56a4e1cee9900
Shahadat Hossain <shahadat3669@gmail.com>
2023-08-20 03:19:11 +0600

    :sparkles: feat(development): add commitlint and cz-customizable and configure

---
+++ .cz-config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  types: [
+    { value: ":sparkles: feat", name: "✨ feat:\tAdding a new feature" },
+    { value: ":fix: fix", name: "🔧 fix:\tFixing a bug" },
+    { value: ":memo: docs", name: "📝 docs:\tAdd or update documentation" },
+    {
+      value: ":style: style",
+      name: "🎨 style:\tAdd or update styles, ui or ux",
+    },
+    {
+      value: ":recycle: refactor",
+      name: "♻️  refactor:\tCode change that neither fixes a bug nor adds a feature",
+    },
+    {
+      value: ":zap: perf",
+      name: "⚡️ perf:\tCode change that improves performance",
+    },
+    {
+      value: ":test: test",
+      name: "🧪 test:\tAdding tests cases",
+    },
+    {
+      value: ":truck: chore",
+      name: "🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation",
+    },
+    { value: ":rewind: revert", name: "⏪️ revert:\tRevert to a commit" },
+    { value: ":construction: wip", name: "🚧 wip:\tWork in progress" },
+    {
+      value: ":construction_worker: build",
+      name: "👷 build:\tAdd or update regards to build process",
+    },
+    {
+      value: ":green_heart: ci",
+      name: "💚 ci:\tAdd or update regards to build process",
+    },
+  ],
+  scopes: [{ name: "readme" }, { name: "development" }],
+  scopeOverrides: {
+    fix: [
+      { name: "merge" },
+      { name: "style" },
+      { name: "test" },
+      { name: "hotfix" },
+    ],
+  },
+  allowCustomScopes: true,
+  allowBreakingChanges: ["feat", "fix", "refactor"],
+  subjectLimit: 100,
+};
