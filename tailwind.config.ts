4b81868ecb861ff065ef5d79cc82bf39b894a074
Shahadat Hossain <shahadat3669@gmail.com>
2023-08-20 12:06:28 +0600

    :art: style(tailwind): add custom colors in the tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,32 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tc: {
+          primary: '#877EFF',
+          secondary: '#FFB620',
+          blue: '#0095F6',
+          red: '#FF5A5A',
+          dark: {
+            '100': '#1F1F22',
+            '200': '#101012',
+            '300': '#121417',
+            '400': '#000000',
+          },
+          light: {
+            '100': '#FFFFFF',
+            '200': '#EFEFEF',
+            '300': '#7878A3',
+            '400': '#5C5C7B',
+          },
+          gray: {
+            '100': '#697C89',
+            '200': 'rgba(16, 16, 18, 0.6)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
