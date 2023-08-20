15c087e7f64f9fe3fef2bff389e447c1d34dd6cc
Shahadat Hossain <shahadat3669@gmail.com>
2023-08-21 01:54:01 +0600

    :art: style(ui): add bottom bar styles for wrapper

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -1,5 +1,13 @@
+'use client';
+
 const BottomBar = () => {
-  return <div>BottomBar</div>;
+  return (
+    <footer className="fixed bottom-0 z-10 w-full rounded-t-3xl bg-tc-dark-300 p-4 px-7 backdrop-blur-lg md:hidden">
+      <div className="gap-3sm:gap-5 flex items-center justify-between text-white">
+        Bottom bar
+      </div>
+    </footer>
+  );
 };
 
 export default BottomBar;
