5d0d9c8f4d090aac9f7a9b51d8dbef83d5c378fc
Shahadat Hossain <shahadat3669@gmail.com>
2023-08-20 12:48:23 +0600

    :art: style(tailwind): add animation and keyframe in tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,8 +36,49 @@ const config: Config = {
         'tc-30': '0px 0px 6px 2px rgba(219, 188, 159, 0.30)',
         'tc-50': '-30px 0px 60px 0px rgba(28, 28, 31, 0.50)',
       },
+      keyframes: {
+        'tc-spin': {
+          to: {
+            transform: 'rotate(360deg)',
+          },
+        },
+        'tc-ping': {
+          '75%, 100%': {
+            transform: 'scale(2)',
+            opacity: '0',
+          },
+        },
+        'tc-pulse': {
+          '50%': {
+            opacity: '.5',
+          },
+        },
+        'tc-bounce': {
+          '0%, 100%': {
+            transform: 'translateY(-25%)',
+            animationTimingFunction: 'cubic-bezier(0.8,0,1,1)',
+          },
+          '50%': {
+            transform: 'none',
+            animationTimingFunction: 'cubic-bezier(0,0,0.2,1)',
+          },
+        },
+        'tc-accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'tc-accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'tc-accordion-down': 'accordion-down 0.2s ease-out',
+        'tc-accordion-up': 'accordion-up 0.2s ease-out',
+      },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+  plugins: [require('tailwindcss-animate')],
 };
 export default config;
