53670a90141a13cf8b6ee46ca1c516795008e33d
Shahadat Hossain <shahadat3669@gmail.com>
2023-08-20 22:29:35 +0600

    :sparkles: feat(ui): add left sidebar item list with responsive styles

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import sidebarLinks from '@/constants';
+
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>;
+  return (
+    <aside className="custom-scrollbar sticky left-0 top-0 z-20 flex h-screen w-fit flex-col justify-between overflow-auto border-r border-r-tc-dark-100 bg-tc-dark-300 pb-5 pt-28 max-md:hidden">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6 text-white">
+        {sidebarLinks.map((link) => (
+          <Link
+            href={link.route}
+            key={link.id}
+            className="relative flex justify-start gap-4 rounded-lg p-4"
+          >
+            <Image src={link.iconURL} alt={link.label} width={24} height={24} />
+            <p className="text-tc-light-100 max-lg:hidden">{link.label}</p>
+          </Link>
+        ))}
+      </div>
+    </aside>
+  );
 };
 
 export default LeftSidebar;
