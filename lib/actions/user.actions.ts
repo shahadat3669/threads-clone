2d5ec2babccd8ed0ae4e515107473f27a8296e27
Shahadat Hossain <shahadat3669@gmail.com>
2023-08-22 00:58:27 +0600

    :sparkles: feat(mongoose): add user actions file with updateUser method which allow to edit or add a new user

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,46 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+
+import User from '../models/user.model';
+import { connectToDB } from '../mongoose';
+
+interface Params {
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+}
+
+export const updateUser = async ({
+  userId,
+  bio,
+  name,
+  path,
+  username,
+  image,
+}: Params): Promise<void> => {
+  try {
+    connectToDB();
+
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboard: true,
+      },
+      { upsert: true },
+    );
+
+    if (path === '/profile/edit') {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
+  }
+};
