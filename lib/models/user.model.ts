aacec2413e5fe0ca01196310f5bb0cb787b8f7ef
Shahadat Hossain <shahadat3669@gmail.com>
2023-08-22 00:47:28 +0600

    :sparkles: feat(mongoose): add user model schema

---
+++ lib/models/user.model.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  bio: String,
+  onboard: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
