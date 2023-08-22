e7bec073294e3786007b199975dcff4db37d68c5
Shahadat Hossain <shahadat3669@gmail.com>
2023-08-23 03:11:55 +0600

    :sparkles: feat: add fetchThreadById and fetchThreads actions to fetch single or all thread

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -31,3 +31,74 @@ export const createThread = async ({ text, author, path }: Params) => {
     throw new Error(`Failed to create thread: ${error.message}`);
   }
 };
+
+export const fetchThreads = async (pageNumber = 1, pageSize = 20) => {
+  connectToDB();
+
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  const threadsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: 'author',
+      model: User,
+    })
+    .populate({
+      path: 'children',
+      populate: {
+        path: 'author',
+        model: User,
+        select: '_id name parentId image',
+      },
+    });
+
+  const totalThreadsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const threads = await threadsQuery.exec();
+
+  const isNext = totalThreadsCount > skipAmount + threads.length;
+
+  return { threads, isNext };
+};
+
+export const fetchThreadById = async (threadId: string) => {
+  connectToDB();
+
+  try {
+    const thread = await Thread.findById(threadId)
+      .populate({
+        path: 'author',
+        model: User,
+        select: '_id id name image',
+      })
+      .populate({
+        path: 'children',
+        populate: [
+          {
+            path: 'author',
+            model: User,
+            select: '_id id name parentId image',
+          },
+          {
+            path: 'children',
+            model: Thread,
+            populate: {
+              path: 'author',
+              model: User,
+              select: '_id id name parentId image',
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (err) {
+    console.error('Error while fetching thread:', err);
+    throw new Error('Unable to fetch thread');
+  }
+};
